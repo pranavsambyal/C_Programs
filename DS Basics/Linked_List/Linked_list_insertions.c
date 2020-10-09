ba0dd47953f1819c6337e5160f920a6e8e93abe8
pranavsambyal <61869021+pranavsambyal@users.noreply.github.com>
2020-10-09 20:40:04 +0530

    trying new gitignore

---
+++ DS Basics/Linked_List/Linked_list_insertions.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+struct node
+{
+    int data;
+    int *next;
+};2
+
+int main()
+{
+    struct node linkedlist[10];
+    printf("Choose the type of insertion you want to use:\n1:Insertion at START\n2:Insertion at MIDDLE\n");
+    printf("3:Insertion at END\n4:Insertion at specified location\n");
+
+}
